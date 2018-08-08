d83d6df0d1fc11106be812de7f2619fdb9f89127
nazmoonnoor <nazmoonnoor@gmail.com>
2018-08-09 00:05:29 +0600

    published folder added

---
+++ Publish/Scripts/app/now.component.js
@@ -0,0 +1,113 @@
+﻿Vue.component('now-grid', {
+        template: '#grid-template',
+        props: {
+            data: Array,
+            columns: Array,
+            filterKey: String
+        },
+        data: function() {
+            var sortOrders = {}
+            this.columns.forEach(function(key) {
+                sortOrders[key] = 1;
+            });
+            return {
+                sortKey: 'Value',
+                sortOrders: sortOrders
+            }
+        },
+        computed: {
+            filteredData: function() {
+                var sortKey = this.sortKey;
+                var filterKey = this.filterKey && this.filterKey.toLowerCase();
+                var order = this.sortOrders[sortKey] || 1;
+                var data = this.data;
+                if (filterKey) {
+                    data = data.filter(function(row) {
+                        return Object.keys(row).some(function(key) {
+                            return String(row[key]).toLowerCase().indexOf(filterKey) > -1;
+                        });
+                    });
+                }
+                if (sortKey) {
+                    data = data.slice().sort(function(a, b) {
+                        a = a[sortKey];
+                        b = b[sortKey];
+                        return (a === b ? 0 : a < b ? 1 : -1) * order;
+                    });
+                }
+                for (var i = 0; i < data.length; i++) {
+                    data[i]["#"] = i + 1;
+                }
+                return data;
+            }
+        },
+        filters: {
+            capitalize: function(str) {
+                return str.charAt(0).toUpperCase() + str.slice(1);
+            }
+        },
+        methods: {
+            sortBy: function(key) {
+                this.sortKey = key;
+                this.sortOrders[key] = this.sortOrders[key] * -1;
+            }
+        }
+    });
+
+// bootstrap the now
+var now = new Vue({
+    el: '#now',
+    data: {
+        items: [],
+        searchQuery: '',
+        gridColumns: ['#', 'Scrip', 'Change', 'Trade', 'Value', 'Volume', 'Spiked', 'Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5'],
+        gridData: [
+        ]
+    },
+    mounted: function () {
+        var self = this;
+        $.ajax({
+            url: '../api/defaultapi/now',
+            method: 'GET',
+            success: function (data) {
+                for (var i = 0; i < data.length; i++) {
+                    data[i]["Volume"] = meaningAmt(data[i]["Volume"]);
+                    if (data[i].Quotes.length > 5) {
+                        data[i]["Spiked"] = data[i]['Value'] * 1000000 > (data[i].Quotes[data[i].Quotes.length - 2].Value + data[i].Quotes[data[i].Quotes.length - 3].Value)
+                            || data[i]['Value'] * 1000000 > (data[i].Quotes[data[i].Quotes.length - 2].Value * 2) ? "YES" : "";
+
+                        data[i]["Day 1"] = meaningAmt(data[i].Quotes[data[i].Quotes.length - 2].Value) + " (" + meaningAmt(data[i].Quotes[data[i].Quotes.length - 2].Volume) + ")";
+                        data[i]["Day 2"] = meaningAmt(data[i].Quotes[data[i].Quotes.length - 3].Value) + " (" + meaningAmt(data[i].Quotes[data[i].Quotes.length - 3].Volume) + ")";
+                        data[i]["Day 3"] = meaningAmt(data[i].Quotes[data[i].Quotes.length - 4].Value) + " (" + meaningAmt(data[i].Quotes[data[i].Quotes.length - 4].Volume) + ")";
+                        data[i]["Day 4"] = meaningAmt(data[i].Quotes[data[i].Quotes.length - 5].Value) + " (" + meaningAmt(data[i].Quotes[data[i].Quotes.length - 5].Volume) + ")";
+                        data[i]["Day 5"] = meaningAmt(data[i].Quotes[data[i].Quotes.length - 6].Value) + " (" + meaningAmt(data[i].Quotes[data[i].Quotes.length - 6].Volume) + ")";
+                    }
+                }
+                self.gridData = data;
+                console.log(data);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+
+        function meaningAmt(labelValue) {
+
+            // Nine Zeroes for Billions
+            return Math.abs(Number(labelValue)) >= 1.0e+9
+
+                ? Math.abs(Number(labelValue)) / 1.0e+9 + "B"
+                // Six Zeroes for Millions 
+                : Math.abs(Number(labelValue)) >= 1.0e+6
+
+                    ? Math.abs(Number(labelValue)) / 1.0e+6 + "M"
+                    // Three Zeroes for Thousands
+                    : Math.abs(Number(labelValue)) >= 1.0e+3
+
+                        ? Math.abs(Number(labelValue)) / 1.0e+3 + "K"
+
+                        : Math.abs(Number(labelValue));
+
+        }
+    }
+})
